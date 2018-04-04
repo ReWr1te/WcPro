1596e56e41ec7ee0b027f7509001841c8415ccac
PaulTian <tsy199707@gmail.com>
2018-04-04 22:26:23 +0800

    Input Handling Function
    Receive the commandline input and read the file accordingly.

--- src/CommandParser.java
+++ src/CommandParser.java
@@ -1,9 +1,53 @@
 public class CommandParser {
     //输入命令行命令，返回文件内容
     public String parseCommand(String[] args) {
-        String content = "";
 
+    	// initiate variables
 
-        return content;
+		// initiate content file, each line, filename and address to store related info
+		String content = "", line = "", addr = "", filename = args[0], os = "";
+
+		// differ operating systems
+		os = System.getProperty("os.name");
+		if (os.indexOf("Mac") != -1)
+			addr = System.getProperty("user.dir") + "/" + filename; // OSX
+		else if (os.indexOf("Win") != -1)
+			addr = System.getProperty("user.dir") + "\\" + filename; // Windows
+
+		// initiate file reader, buffered reader and string buffer to store file medially
+		FileReader fileReader = null;
+		BufferedReader bufferedReader = null;
+		StringBuffer stringBuffer = new StringBuffer();
+
+		// begin reading
+		try {
+
+			// read file according to address
+			fileReader = new FileReader(addr);
+			bufferedReader = new BufferedReader(fileReader);
+
+			// read all lines
+			while ((line = bufferedReader.readLine()) != null) {
+				stringBuffer.append(line);
+				stringBuffer.append('\n'); // add \n to go to next line
+			}
+			
+			content = stringBuffer.toString(); // change file contents to the type of String
+
+		} catch (FileNotFoundException e) { // to catch exceptions
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+        	if (bufferedReader != null) { // to check whether the buffered reader is closed
+                try {
+                    bufferedReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+		return content; // to return the result
     }
 }
